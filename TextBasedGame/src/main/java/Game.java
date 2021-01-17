e1141014d1c56eb95f5add99e2f2a7d0d0cf3e73
RoidAkbar <63292984+RoidAkbar@users.noreply.github.com>
2021-01-17 22:12:00 +0800

    Nyoba Dulu
    Yang penting bisa di kerjain bersama dulu

---
+++ TextBasedGame/src/main/java/Game.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class Game {
+
+    private final Player player = Player.newInstance();
+
+    public void play() throws IOException {
+        System.out.println("You are " + player + " " + player.getDescription());
+        Dungeon.newInstance().startQuest(player);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.play();
+    }
+
+}
--- TextBasedGame/src/main/java/Player.java
+++ TextBasedGame/src/main/java/Player.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.textbasedgame;
-import java.util.*;
-/**
- *
- * @author hydro
- */
+import java.util.Random;
+
 public class Player {
+
     private final String name;
     private final String description;
     private final int maxHitPoints;
